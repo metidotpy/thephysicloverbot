1c55a57b515a56968961709710a23179dfd9d1e6
Mehdi Radfar <metiii.py@gmail.com>
2023-06-19 03:55:00 +0330

    add book and change it to mysql

--- database/tables.py
+++ database/tables.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine, Column, String, Table, Integer, DateTime, Boolean, Text, ForeignKey, func
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from decouple import config
 
+USERNAME_DATABASE = config("USERNAME_DATABASE")
+PASSWORD_DATABASE = config("PASSWORD_DATABASE")
+ADDRESS_DATABASE = config("ADDRESS_DATABASE")
+DATABASE_NAME = config("DATABASE_NAME")
 
-engine = create_engine("sqlite:///thephysicloverbot.sqlite")
+url = f"mysql://{USERNAME_DATABASE}:{PASSWORD_DATABASE}@{ADDRESS_DATABASE}/{DATABASE_NAME}"
+
+engine = create_engine(url)
 Base = declarative_base(bind=engine)
 Session = sessionmaker(bind=engine)()
 
@@ -15,9 +22,9 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key = True, unique=True, nullable=False, autoincrement=True)
     chat_id = Column(String(16), unique=True, nullable=False)
-    first_name = Column(String())
-    last_name = Column(String())
-    username = Column(String())
+    first_name = Column(String(60))
+    last_name = Column(String(60))
+    username = Column(String(75))
     is_blocked = Column(Boolean, default=False)
     messages = relationship("Message", backref="message")
     created = Column(DateTime(timezone=True), default=func.now())
@@ -41,6 +48,13 @@ class Music(Base):
 
 class Movie(Base):
     __tablename__ = "movie"
+    id = Column(Integer, primary_key = True, unique=True, nullable=False, autoincrement=True)
+    message_id = Column(Integer, unique=True)
+    created = Column(DateTime(timezone=True), default=func.now())
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
+
+class Book(Base):
+    __tablename__ = "book"
     id = Column(Integer, primary_key = True, unique=True, nullable=False, autoincrement=True)
     message_id = Column(Integer, unique=True)
     created = Column(DateTime(timezone=True), default=func.now())
