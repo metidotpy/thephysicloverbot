edf2c2078a8575a16de7e496841ca698979f5085
Mehdi Radfar <metiii.py@gmail.com>
2023-06-19 03:55:03 +0330

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -6,16 +6,18 @@ from pyrogram.types import (
 
 MAIN_USER_KEYBOARD = ReplyKeyboardMarkup(
     keyboard= [
-        ['معرفی فیلم 🎥'],
-        ['معرفی موزیک 🎵'],
+        [
+            'معرفی موزیک 🎵',
+            'معرفی فیلم 🎥',
+            'معرفی کتاب 📕'
+        ],
     ],
     resize_keyboard=True
 )
 
 MAIN_ADMIN_KEYBOARD = ReplyKeyboardMarkup(
     keyboard= [
-        ['معرفی فیلم 🎥'],
-        ['معرفی موزیک 🎵'],
+        ['معرفی موزیک 🎵','معرفی فیلم 🎥','معرفی کتاب 📕'],
         ['لیست کاربر ها 👤', "لیست ادمین ها 👤", "لیست پیام ها 💬"],
     ],
     resize_keyboard=True
