31eabb8e62e96e80285e263dc283f966ac0dd91b
Mehdi Radfar <81019265+metidotpy@users.noreply.github.com>
2023-06-22 01:54:53 +0330

    Update var.py

--- var.py
+++ var.py
@@ -3,6 +3,6 @@ from decouple import config
 API_ID = config("api_id")
 API_HASH = config("api_hash")
 BOT_TOKEN = config("bot_token")
-MUSIC_CHANNEL_ID = -1001628724625
-MOVIE_CHANNEL_ID = -1001764446589
-BOOK_CHANNEL_ID = -1001923423418
+MUSIC_CHANNEL_ID = "your channel id"
+MOVIE_CHANNEL_ID = "your channel id"
+BOOK_CHANNEL_ID = "your channel id"
